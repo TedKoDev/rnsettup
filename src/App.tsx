733197ed4e91f1c61893267283a71804d90bac7d
홍태의 <hongtaeui@hongtaeuis-MacBook-Pro.local>
2023-10-15 15:20:05 +0900

    settup done

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Div, Text } from 'react-native-magnus'
+
+export default function COMPONENT_NAME() {
+  return (
+    <Div>
+      <Text>dfs</Text>{' '}
+    </Div>
+  )
+}
